f0e668d8f9d0b173037af067d78b243b886f91c0
Brian R. Wittig <brian@brianwittig.com>
2025-06-14 08:17:03 -0400

    app commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from models import Session, Job
+import os
+
 
 app = Flask(__name__)
 
@@ -17,4 +19,5 @@ def index():
     return render_template("index.html", jobs=jobs, query=query, tag=tag)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
